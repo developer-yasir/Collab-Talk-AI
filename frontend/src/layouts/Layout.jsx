66dfc896f1f85309f67f15b1947d9580755291b3
Yasir Raees <146189016+developer-yasir@users.noreply.github.com>
2025-12-11 17:13:56 +0500

    fix: remove overflow-hidden to resolve layout export issue

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -4,7 +4,7 @@ import Sidebar from '../components/Sidebar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex h-screen bg-gray-50 overflow-hidden">
+    <div className="flex h-screen bg-gray-50">
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0"> {/* min-w-0 helps with flexbox shrinking behavior */}
         <Navbar />
